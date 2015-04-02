40566e1795c88d3a5f3a442d59ac98d3cf3d9a1f
Thomas Huraux <thomas.huraux@gmail.com>
2015-04-02 19:39:24 +0200

    Adding PotentialHeatmap
    + SDL 2
    + PotentialHeatmap

---
+++ Source/PotentialHeatmap.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "SDL.h"
+#include "Map.h"
+#include "Camera.h"
+
+class PotentialHeatmap {
+
+public:
+  PotentialHeatmap(unsigned int width, unsigned int height);
+  virtual ~PotentialHeatmap();
+  void update(Map * map, Camera * camera);
+
+private:
+  SDL_Window * _window;
+  SDL_Renderer * _renderer;
+  SDL_Event _event;
+
+  int _width, _height, _tileW, _tileH;
+
+  SDL_Color potentialToColor(double potential);
+
+};
